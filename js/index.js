17d37a7aff4fa97dd746acf396852795f2b91f42
Thrashskin <45271203+Thrashskin@users.noreply.github.com>
2020-10-27 00:29:03 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ window.onload = () => {
             ctx.drawImage(backGroundImg, 0, 0, canvas.width, canvas.height);
         };
 
-    backGroundImg.src = './images/sky.jpg';
+    backGroundImg.src = 'https://github.com/Thrashskin/thors-odyssey/blob/gh-pages/images/sky.jpg';
 
     document.getElementById('start-button').addEventListener('click', () => {
         startGame();
